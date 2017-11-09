45a8016c88723e23d81543e474ce43674327d261
Ant <350969089@qq.com>
2017-11-09 17:33:58 +0800

    PCA 2017/11/9

--- PCA/PCA_Test.py
+++ PCA/PCA_Test.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
     PcaClass.call_PCA()
 
     # 路径
-    dataFile = './iris.data.txt'
+    dataFile = './data/testSet.txt'
 
-    # 查看原始数据 第一维与第二维的数据图像, loadData 函数有两个参数，第二个参数为分隔符
-    rawData = PcaClass.load_data(dataFile,',')
-    PcaClass.DrawPic(rawData,2,3)
+    # 查看原始数据 第一维与第二维的数据图像, loadData 函数有两个参数，第二个参数为分隔符（默认为\t）
+    rawData = PcaClass.load_data(dataFile)
+    PcaClass.DrawPic(rawData,0,1)
 
     # 查看PCA后（正则化以及扩展到原始维度进行样本比较,即观察降维后的模型）
-    PCA_Data,PCA_Pic_Data = PcaClass.StepByStep_PCA(rawData,3)
-    PcaClass.DrawPic(PCA_Pic_Data,2,3)
+    PCA_Data,PCA_Pic_Data = PcaClass.StepByStep_PCA(rawData,1)
+    PcaClass.DrawPic(PCA_Pic_Data,0,1)
